27facb7cb6e2cf74c32fef3b23e5ce40b7d2b775
Munteanu <Dragos.Munteanu@agro.local>
2026-02-20 15:25:33 +0200

    terminat manuale semanatori

--- src/data/kverneland-data.js
+++ src/data/kverneland-data.js
@@ -44,6 +44,13 @@ export const kvernelandData = {
                         versiuni: [
                              { an: "Model 2019", link: "https://drive.google.com/uc?export=download&id=1812AyEUy-Q4Nau1JYtODln9PwYypcgK9" }
                         ]
+                    },
+                    { 
+                        nume: "U-Drill/U-Drill Plus", 
+                        descriere: "Manual Service",
+                        versiuni: [
+                            { an: "U-Drill/U-Drill Plus", link: "https://drive.google.com/uc?export=download&id=1fe333PB4z-UcnmFCg_FzpjQLW-Xpumn7" }
+                        ] 
                     }
                 ]
             },
@@ -156,6 +163,138 @@ export const kvernelandData = {
                                 fisiere: [
                                     { nume: "Manual Operator", link: "https://drive.google.com/uc?export=download&id=1ky1Blv_uuOATYru8F5wBE0Kp-E1XEsG1" },
                                     { nume: "Manual Terminal", link: "https://drive.google.com/uc?export=download&id=11bfTBy8qHSeXJE_BhalZYT8ozhoNsBmS" }
+                                ]
+                            } 
+                        ]
+                    },
+                    { 
+                        nume: "Satio", 
+                        descriere: "Ghid Operator", 
+                        versiuni: [ 
+                            { 
+                                an: "Ghid RO", 
+                                isFolder: true, 
+                                fisiere: [
+                                    { nume: "Manual Operator", link: "https://drive.google.com/file/d/1FnTCongfM_RZWDKWCl8fo8WkjRT6Y2Bq/view?usp=drive_link" }
+                                ]
+                            }, 
+                            { 
+                                an: "Ghid EN", 
+                                isFolder: true,
+                                fisiere: [
+                                    { nume: "Manual Operator", link: "https://drive.google.com/file/d/1g5VJZVjEbeo_cizijngOi4KrY9JMeiCx/view?usp=drive_link" }
+                                ]
+                            } 
+                        ]
+                    },
+                    { 
+                        nume: "Satio F", 
+                        descriere: "Ghid Operator", 
+                        versiuni: [ 
+                            { 
+                                an: "Ghid RO", 
+                                isFolder: true, 
+                                fisiere: [
+                                    { nume: "Manual Operator", link: "https://drive.google.com/file/d/11SI0-3aAfxAtQBuzd12YCJYQP0kuBzpj/view?usp=drive_link" },
+                                    { nume: "Manual Terminal E-COM ISOBUS", link: "https://drive.google.com/file/d/1673nR5n8LirYIffNfnV2oouNgBQylwWA/view?usp=drive_link" },
+                                    { nume: "Manual Terminal E-BAS", link: "https://drive.google.com/file/d/1OxZBjt03UuVGZSaMGN69ZiwGtdV3GJwd/view?usp=drive_link" }
+                                ]
+                            },
+                            { 
+                                an: "Ghid EN", 
+                                isFolder: true,
+                                fisiere: [
+                                    { nume: "Manual Operator", link: "https://drive.google.com/file/d/10IdUm-JiPYNtJjaPdfmgb6LHGDX3I1wY/view?usp=drive_link" },
+                                    { nume: "Manual Terminal E-COM ISOBUS", link: "https://drive.google.com/file/d/1Lgyk6zHiByybXl2ftspDTX8f8bO9jARn/view?usp=drive_link" },
+                                    { nume: "Manual Terminal E-BAS", link: "https://drive.google.com/file/d/1j7j7ZfiyAT4xw1QN4uqbhGM_NkGZDUXf/view?usp=drive_link" }
+                                ]
+                            }
+                        ]
+                    }, 
+                    { 
+                        nume: "U-Drill 3-4m", 
+                        descriere: "Ghid Operator", 
+                        versiuni: [ 
+                            { 
+                                an: "Ghid RO", 
+                                isFolder: true, 
+                                fisiere: [
+                                    { nume: "Manual Operator", link: "https://drive.google.com/file/d/1py4gyKIi7QzpzK5UUC6Z7A0Jz2WbaKHx/view?usp=drive_link" },
+                                    { nume: "Manual Terminal", link: "https://drive.google.com/file/d/1BGwO5GSiFAQ9pf7v2OwyD1poR29wC-bv/view?usp=drive_link" }
+                                ]
+                            }, 
+                            { 
+                                an: "Ghid EN", 
+                                isFolder: true,
+                                fisiere: [
+                                    { nume: "Manual Operator", link: "https://drive.google.com/file/d/1pl-mPJGm9u2l4vZ_Mchp_gg7nlyeSL3I/view?usp=drive_link" },
+                                    { nume: "Manual Terminal", link: "https://drive.google.com/file/d/1uQHyUjSNTFDJBDJwujUCn-ciKDWvL2Cn/view?usp=drive_link" }
+                                ]
+                            } 
+                        ]
+                    },
+                    { 
+                        nume: "U-Drill 6m", 
+                        descriere: "Ghid Operator", 
+                        versiuni: [ 
+                            { 
+                                an: "Ghid RO", 
+                                isFolder: true, 
+                                fisiere: [
+                                    { nume: "Manual Operator", link: "https://drive.google.com/file/d/1JBAdGAyulNUKoateDvCV58p29WWQ3qso/view?usp=drive_link" },
+                                    { nume: "Manual Terminal", link: "https://drive.google.com/file/d/1BGwO5GSiFAQ9pf7v2OwyD1poR29wC-bv/view?usp=drive_link" }
+                                ]
+                            }, 
+                            { 
+                                an: "Ghid EN", 
+                                isFolder: true,
+                                fisiere: [
+                                    { nume: "Manual Operator", link: "https://drive.google.com/file/d/1oEqISXeMJAu25L7_iIKyEvKWldRx4kem/view?usp=drive_link" },
+                                    { nume: "Manual Terminal", link: "https://drive.google.com/file/d/1uQHyUjSNTFDJBDJwujUCn-ciKDWvL2Cn/view?usp=drive_link" }
+                                ]
+                            } 
+                        ]
+                    },
+                    { 
+                        nume: "U-Drill Plus 3-4m", 
+                        descriere: "Ghid Operator", 
+                        versiuni: [ 
+                            { 
+                                an: "Ghid RO", 
+                                isFolder: true, 
+                                fisiere: [
+                                    { nume: "Manual Operator", link: "https://drive.google.com/file/d/1n7psk4jKCKcGItUI_QkSXgxvif5nedhp/view?usp=drive_link" },
+                                    { nume: "Manual Terminal", link: "https://drive.google.com/file/d/1BGwO5GSiFAQ9pf7v2OwyD1poR29wC-bv/view?usp=drive_link" }
+                                ]
+                            }, 
+                            { 
+                                an: "Ghid EN", 
+                                isFolder: true,
+                                fisiere: [
+                                    { nume: "Manual Operator", link: "https://drive.google.com/file/d/1IWQrHtmyD3g1m088cxHSaSjYO3TP2hau/view?usp=drive_link" },
+                                    { nume: "Manual Terminal", link: "https://drive.google.com/file/d/1uQHyUjSNTFDJBDJwujUCn-ciKDWvL2Cn/view?usp=drive_link" }
+                                ]
+                            } 
+                        ]
+                    },
+                    { 
+                        nume: "U-Drill Plus 6m", 
+                        descriere: "Ghid Operator", 
+                        versiuni: [ 
+                            { 
+                                an: "Ghid RO", 
+                                isFolder: true, 
+                                fisiere: [
+                                    { nume: "Manual Operator", link: "https://drive.google.com/file/d/1jiItMG8O-bmrqEjRKI7EUQNMSAFqMCLY/view?usp=drive_link" },
+                                    { nume: "Manual Terminal", link: "https://drive.google.com/file/d/1BGwO5GSiFAQ9pf7v2OwyD1poR29wC-bv/view?usp=drive_link" }
+                                ]
+                            }, 
+                            { 
+                                an: "Ghid EN", 
+                                isFolder: true,
+                                fisiere: [
+                                    { nume: "Manual Operator", link: "https://drive.google.com/file/d/1wEU1bGre8Q4PvSoebvh1ms9rmPE7U8kf/view?usp=drive_link" },
+                                    { nume: "Manual Terminal", link: "https://drive.google.com/file/d/1uQHyUjSNTFDJBDJwujUCn-ciKDWvL2Cn/view?usp=drive_link" }
                                 ]
                             } 
                         ]
