1b08e5e49daa754416922fd777d4742a997d1bcb
Munteanu <Dragos.Munteanu@agro.local>
2026-09-18 15:01:54 +0300

    am adaugat useri noi

---
+++ src/data/tic-data.js
@@ -0,0 +1,94 @@
+export const ticData = {
+    "2026": {
+        title: "2026",
+        resources: {
+            "combine": {
+                title: "Combine",
+                models: [
+                    { nume: "TC EU – Importeri (Februarie)", link: "https://drive.google.com/file/d/1IC0b89R59ag3wk-x_78onZTJlU-kbzyh/view?usp=drive_link" },
+                    { nume: "CX MR – Importeri (Februarie)", link: "https://drive.google.com/file/d/1UyEL9XWng-qdj79jTqMrUaR1TdIyfb_I/view?usp=drive_link" },
+                    { nume: "Harvest, Hay & Forage Engines – Importeri", link: "https://drive.google.com/file/d/1LWaeTosZ0iY8fu3I2AJJDvX6N7JubppF/view?usp=drive_link" },
+                    { nume: "CX/CR Flagship – Importeri", link: "https://drive.google.com/file/d/1MVepmHxptp7A2fMveZoXcwYH931tmvfS/view?usp=drive_link" },
+                    { nume: "Next Gen CR & AF Combines", link: "https://drive.google.com/file/d/1GkUZ6e4Eu9n7JvlqQjlgtWVks7ERf8Eq/view?usp=drive_link" }
+                ]
+            },
+            "tractoare": {
+                title: "Tractoare",
+                models: [
+                    { nume: "T4 T5 – Ankara Products Importer", link: "https://drive.google.com/file/d/1MImShgXcBA7KmAUBu2Pw0_jXwZkhg9N_/view?usp=drive_link" },
+                    { nume: "T4 T5 – UTL Light Erenler & Medium Jesi Importeri", link: "https://drive.google.com/file/d/1aqyIWV8qG7vwX9kgul8wvkgjPQnB7nA5/view?usp=drive_link" },
+                    { nume: "T4F – Specialty and Crawler Importer", link: "https://drive.google.com/file/d/1AjRmZYrfIdsK1x4HMtWDELCSu9uo7hwb/view?usp=drive_link" },
+                    { nume: "T5 – APL SH Jesi Importeri", link: "https://drive.google.com/file/d/1rRaA_pqH5tFQqxMn7_UBZfBMNxNOKF7Q/view?usp=drive_link" },
+                    { nume: "T5 – APL16x16 Jesi Importeri", link: "https://drive.google.com/file/d/1iILjaHadlLV8gvlJj416qHOckbcwSQVH/view?usp=drive_link" },
+                    { nume: "T6 – APH Importer", link: "https://drive.google.com/file/d/1NleaPs1j_YeuFIY6M2yzbl5JSr8gKsfw/view?usp=drive_link" },
+                    { nume: "T7 – CCM LWB Import", link: "https://drive.google.com/file/d/1bWapzrK1iQbRIHAFqir05JPmp29f66wJ/view?usp=drive_link" },
+                    { nume: "T7 – SWB Importer", link: "https://drive.google.com/file/d/1nqm_K5FOBEa_VSrit6EyJJEZgSJ-KrhP/view?usp=drive_link" },
+                    { nume: "T7HD – CCM HD Import", link: "https://drive.google.com/file/d/1yjW9UVDV8ynNJhEN65Y2o_abKP5i3X7T/view?usp=drive_link" },
+                    { nume: "T8 – CCH Importer Session", link: "https://drive.google.com/file/d/1sbhEitocJVnYXGeEvBoGpVJs_0lZkAVv/view?usp=drive_link" }
+                ]
+            }
+        }
+    },
+    "2025": {
+        title: "2025",
+        resources: {
+            "combine": {
+                title: "Combine",
+                models: [
+                    { nume: "CR – Importer", link: "https://drive.google.com/file/d/1vSzujs27ssrbBQaypCVMh7gjVdkvNaCF/view?usp=drive_link" },
+                    { nume: "CX56/CH – Importeri", link: "https://drive.google.com/file/d/1TcG8sw_4dhlUTVWYOCuv8eoexIxMu2lR/view?usp=drive_link" },
+                    { nume: "CX7/8 – Importeri", link: "https://drive.google.com/file/d/1mIFkmCLF_vT-ndRAmz-nQDwBIs4rr_mt/view?usp=drive_link" },
+                    { nume: "Engines – Importeri", link: "https://drive.google.com/file/d/1tkfmL6z07ubbHkqEBXGgrUoosf_g46Jh/view?usp=drive_link" },
+                    { nume: "TC EU – Februarie", link: "https://drive.google.com/file/d/1kKkLsl1GD2PTq3ywAqP8W6tCN_gXWyuY/view?usp=drive_link" }
+                ]
+            },
+            "tractoare": {
+                title: "Tractoare",
+                models: [
+                    { nume: "T4 T5 – Ankara Products Importer", link: "https://drive.google.com/file/d/1SawzVVGwUzVCkGk2_3YiqKXzxin54-Aq/view?usp=drive_link" },
+                    { nume: "T4 T5 – Utility Light & Medium Importeri", link: "https://drive.google.com/file/d/1bChU5gL7pza6Grdckfjr74lHOQyJ71Up/view?usp=drive_link" },
+                    { nume: "T4F – Specialty and Crawler Importer", link: "https://drive.google.com/file/d/1PB9jwkNu_y6R4p0fhYAOvg9EXdwX18h3/view?usp=drive_link" },
+                    { nume: "T5 – APL16x16 & 32x32 Importeri", link: "https://drive.google.com/file/d/1aMMgJenEnKiEGt2y8C4p0lvUQz5RrB8t/view?usp=drive_link" },
+                    { nume: "T6 – APH Importer", link: "https://drive.google.com/file/d/1fRdOGXonGMs3I4BCDCvmsxOiRlhl6dB5/view?usp=drive_link" },
+                    { nume: "T7 – CCM LWB & CCM LWB MJ22", link: "https://drive.google.com/file/d/1Ew-a-TTjjxPqwOrxVL36a5Ee3ejTHZ_c/view?usp=drive_link" },
+                    { nume: "T7 – SWB Importer", link: "https://drive.google.com/file/d/1e_7R3jfCiqa9XIuNXmG3qX-FsNAq5CyX/view?usp=drive_link" },
+                    { nume: "T7HD – CCM HD Importer", link: "https://drive.google.com/file/d/1lKEEHR9BKz4lu2Wou1O2lId3aSkPVGUq/view?usp=drive_link" },
+                    { nume: "T8 – CCH", link: "https://drive.google.com/file/d/1rYrtRlT0xgxPYS_shdZ_fkoaPTEeYup6/view?usp=drive_link" },
+                    { nume: "Cursor Engines – Importeri", link: "https://drive.google.com/file/d/1Uxs_V9Kolhl1h7Y6A6IwL7OdP6P7agUX/view?usp=drive_link" },
+                    { nume: "Engines F5-NEF – Importeri", link: "https://drive.google.com/file/d/1zIY1sbxehdQAqXe4TfAg_Mu-tU5zs4TR/view?usp=drive_link" }
+                ]
+            }
+        }
+    },
+    "2024": {
+        title: "2024",
+        resources: {
+            "combine": {
+                title: "Combine",
+                models: [
+                    { nume: "Engines NEF & Cursor – Importeri", link: "https://drive.google.com/file/d/1RJ_wE8bn2XES90syh_ObndKJYOYg-nwy/view?usp=drive_link" },
+                    { nume: "TC Combine", link: "https://drive.google.com/file/d/1CEiDPA5Cz82NcuV7j8JESYFD382A7gpH/view?usp=drive_link" },
+                    { nume: "CX56CH", link: "https://drive.google.com/file/d/1Fq-4qB4flwFQlSmWXlaHlyPZmJX0C2R-/view?usp=drive_link" },
+                    { nume: "CX78", link: "https://drive.google.com/file/d/15LhomWzZcEiEnXgQqrhBXHqZfrDWQ6oi/view?usp=drive_link" },
+                    { nume: "CR", link: "https://drive.google.com/file/d/1O9tTE4HE8Ep_GDbh8AplFn3BFsAypg2-/view?usp=drive_link" }
+                ]
+            },
+            "tractoare": {
+                title: "Tractoare",
+                models: [
+                    { nume: "T4 – Utility Platforms Importeri", link: "https://drive.google.com/file/d/1RXohgWekGi9KDrUNnP8O9Q8UnRzOxf6x/view?usp=drive_link" },
+                    { nume: "T4 T5 – Utility APL Common Importeri", link: "https://drive.google.com/file/d/1azgUML1WCvvHNxGlttzQi--4EJkhN4FX/view?usp=drive_link" },
+                    { nume: "T4F – Specialty and Crawler Importer", link: "https://drive.google.com/file/d/1ET2lb9wjTpOHzH8mBO8GJKcXWR911uRY/view?usp=drive_link" },
+                    { nume: "T5 – Ankara Products Importer", link: "https://drive.google.com/file/d/1Aw5Qe3xMJ6mFGZPgpuTE8joR1FtPTyuU/view?usp=drive_link" },
+                    { nume: "T5 – APL Platforms 16x16 & 32x32 Importeri", link: "https://drive.google.com/file/d/19DWz5rmvRahe3i5FsSsM6108usLRfU6P/view?usp=drive_link" },
+                    { nume: "T6 – APH Importer", link: "https://drive.google.com/file/d/15z_PLA-OnsSVCBzhlZ9jeis1RX0-TfUo/view?usp=drive_link" },
+                    { nume: "T7 – CCM LWB & LWB New Cab Importer", link: "https://drive.google.com/file/d/1knd-TcXNzMZbB7_lNongpuXXIJxlWXsm/view?usp=drive_link" },
+                    { nume: "T7 – SWB Importeri", link: "https://drive.google.com/file/d/1hXr13Ze_SlJjU5IACm1DrHLEtko01flm/view?usp=drive_link" },
+                    { nume: "T7HD – CCM HD Importeri", link: "https://drive.google.com/file/d/1KTMDdX-5ofNlggofBFbIKMDI7RqRtSCQ/view?usp=drive_link" },
+                    { nume: "T8 – CCH Importer", link: "https://drive.google.com/file/d/1fh_LI31GGfPDEJ1Jw_yHky1QrGL3LgxL/view?usp=drive_link" },
+                    { nume: "Engines F5, NEF, Cursor", link: "https://drive.google.com/file/d/1kjNzURwNNernmp0PLBjot6QdT9NyT6Ay/view?usp=drive_link" }
+                ]
+            }
+        }
+    }
+};
--- src/data/tractoare-data.js
+++ src/data/tractoare-data.js
@@ -2,67 +2,74 @@ export const tractoareData = {
     "t4": {
         title: "Seria T4",
         resources: {
-            "manual-service": { 
-                title: "Manual Service", 
+            "material-training": {
+                title: "Material Training",
                 models: [
-                    // Exemplu de adăugare manual
-                    { 
-                        nume: "T4 F/N/V", 
-                        descriere: "Manual Service",
-                        versiuni: [
-                            { an: "Panou sigurante fuzibile", link: "LIPEȘTE_LINK_AICI" }
-                        ] 
-                    },
-                    { 
-                        nume: "T4.75S", 
-                        descriere: "Manual Service",
-                        versiuni: [
-                            { an: "Panou sigurante fuzibile", link: "LIPEȘTE_LINK_AICI" }
-                        ] 
-                    },
-                    
-
-                ] 
+                    { nume: "T4 F/N/V", link: "LIPEȘTE_LINK_AICI" },
+                    { nume: "T4.75S", link: "LIPEȘTE_LINK_AICI" }
+                ]
             },
-            "manual-operare": { title: "Manual Operare", models: [] },
-            "video": { title: "Tutorial Video", models: [] },
-            "erori": { title: "Coduri de Eroare", models: [] }
+            "manual-operare": { title: "Manual Operare", models: [] }
         }
     },
     "t5": {
         title: "Seria T5",
         resources: {
-            "manual-service": { title: "Manual Service", models: [] },
-            "manual-operare": { title: "Manual Operare", models: [] },
-            "video": { title: "Tutorial Video", models: [] },
-            "erori": { title: "Coduri de Eroare", models: [] }
+            "material-training": { title: "Material Training", models: [] },
+            "manual-operare": { title: "Manual Operare", models: [] }
         }
     },
     "t6": {
         title: "Seria T6",
         resources: {
-            "manual-service": { title: "Manual Service", models: [] },
-            "manual-operare": { title: "Manual Operare", models: [] },
-            "video": { title: "Tutorial Video", models: [] },
-            "erori": { title: "Coduri de Eroare", models: [] }
+            "material-training": { title: "Material Training", models: [] },
+            "manual-operare": { title: "Manual Operare", models: [] }
         }
     },
     "t7": {
         title: "Seria T7",
         resources: {
-            "manual-service": { title: "Manual Service", models: [] },
-            "manual-operare": { title: "Manual Operare", models: [] },
-            "video": { title: "Tutorial Video", models: [] },
-            "erori": { title: "Coduri de Eroare", models: [] }
+            "material-training": { title: "Material Training", models: [] },
+            "manual-operare": { title: "Manual Operare", models: [] }
         }
     },
     "t8": {
         title: "Seria T8",
         resources: {
-            "manual-service": { title: "Manual Service", models: [] },
-            "manual-operare": { title: "Manual Operare", models: [] },
-            "video": { title: "Tutorial Video", models: [] },
-            "erori": { title: "Coduri de Eroare", models: [] }
+            "material-training": {
+                title: "Material Training",
+                models: [
+                    { nume: "CNH CP12 Overview", link: "https://drive.google.com/file/d/1sV23cKbFYuePomaEWQaP_4c4qMZNTDo0/view?usp=drive_link" },
+                    { nume: "CNH Phoenix Setup", link: "https://drive.google.com/file/d/18dMyc6WhoZSzIJkaTs96T-AkVccKOUs1/view?usp=drive_link" },
+                    { nume: "CCH Stage V", link: "https://drive.google.com/file/d/1B49ogIjO54nP1qu2KL426k0pf36DuOQO/view?usp=drive_link" },
+                    { nume: "eVGT Cursor9", link: "https://drive.google.com/file/d/1Ef8c59PUeG-SA12CocWZretvK1-5yrpx/view?usp=drive_link" },
+                    { nume: "Powershift Transmission MY25", link: "https://drive.google.com/file/d/1pY9etgnEs0FqGrsLD_oP43U5_h7iq2ue/view?usp=drive_link" },
+                    { nume: "CH28 CVT Transmission", link: "https://drive.google.com/file/d/1vWnuRJeDyviXSAaTmtotOnRzMSTNjPQ5/view?usp=drive_link" },
+                    { nume: "Axle", link: "https://drive.google.com/file/d/1W7h6AEPyOEZJmmvf9TLcow-XWwACZAHq/view?usp=drive_link" },
+                    { nume: "MFD", link: "https://drive.google.com/file/d/18BzCkd_0D-Z-VBfnK4xk1B5sEhfxNc5q/view?usp=drive_link" },
+                    { nume: "Rubber Tracks", link: "https://drive.google.com/file/d/1NwnLhKTW_Hjxfnr1KGy3wYLt7YjW6F75/view?usp=drive_link" },
+                    { nume: "CCH Brakes", link: "https://drive.google.com/file/d/1k5E3Gf-skrTQ43giKil_w_DZCA9mhSg8/view?usp=drive_link" },
+                    { nume: "Hydraulic System", link: "https://drive.google.com/file/d/1r6HNmM7VyXTsMBzFHTBnfFLPggyTEZN1/view?usp=drive_link" },
+                    { nume: "CCH FEHR Valves", link: "https://drive.google.com/file/d/1QL2SXt_dTldwwFhyrZww4MiQ5a9utXWR/view?usp=drive_link" },
+                    { nume: "CCH Danfoss Valves", link: "https://drive.google.com/file/d/1-JTG2DhxuA0oU9KiGsKj-BLXSxzSpH_H/view?usp=drive_link" },
+                    { nume: "FSUS", link: "https://drive.google.com/file/d/1C_WX3Y7uZhUBcRLFZgpxOiJ0Ox2l8xu7/view?usp=drive_link" },
+                    { nume: "Active Cab Suspension", link: "https://drive.google.com/file/d/1zpCuLAZHvcZWmzh3wd1h4pWvuh0SFYUS/view?usp=drive_link" },
+                    { nume: "Hydraulic Schematic PST", link: "https://drive.google.com/file/d/1E079wJS7LrNOrpD3Nw5LBxTX9Kh5_OCQ/view?usp=drive_link" },
+                    { nume: "Hydraulic Schematic CVT", link: "https://drive.google.com/file/d/1bQ-uSGmCKpfe8LJtxW0wqi5pDcRvDXw3/view?usp=drive_link" },
+                    { nume: "CIH FH and FPTO", link: "https://drive.google.com/file/d/1EdpDT5KRrDRX63arM4qIPTMqwNgnZ880/view?usp=drive_link" },
+                    { nume: "NH FH and PTO", link: "https://drive.google.com/file/d/1JukBmen7ZRLlmvTjGm2Yv7WB7mQOtEN9/view?usp=drive_link" },
+                    { nume: "Rear PTO", link: "https://drive.google.com/file/d/1Hf52__fHbFKArzr4Us3ybQdGYtYGc5Sp/view?usp=drive_link" },
+                    { nume: "CCH Hitch Danfoss", link: "https://drive.google.com/file/d/1_sPCRMuAQTTprN5KBYQejWYLD_kuoj_C/view?usp=drive_link" },
+                    { nume: "CCH CP12 Electrical System", link: "https://drive.google.com/file/d/1HYvCaWyhZgTzD7RGqc8KhZJQ3kffsyNl/view?usp=drive_link" },
+                    { nume: "Fuses and Relays CH28", link: "https://drive.google.com/file/d/1CmMGZkB8GNSvXJQcT2lCPJ5KEGyO_zJ6/view?usp=drive_link" },
+                    { nume: "CCH CP12 EST", link: "https://drive.google.com/file/d/1fIQCp439GpWZ1V6fd_6T0L_6Goea_Ifw/view?usp=drive_link" },
+                    { nume: "Remote Service Tool", link: "https://drive.google.com/file/d/1Qdwnk7OwvtoHf1bt1-xLj_Mi0PFvwAJ-/view?usp=drive_link" },
+                    { nume: "CNH HVAC", link: "https://drive.google.com/file/d/1W5QjHp7wkyLE8-IBJTTL2fFGxHAmtCtF/view?usp=drive_link" },
+                    { nume: "TPMS CCH MY25", link: "https://drive.google.com/file/d/18Ya9gzcMb5wF50W4dafB6dIXFKvGU5ZG/view?usp=drive_link" },
+                    { nume: "Onboard Calibration", link: "https://drive.google.com/file/d/1Hood6YLwMgZFf708_qLkEy1gE1UAddm1/view?usp=drive_link" }
+                ]
+            },
+            "manual-operare": { title: "Manual Operare", models: [] }
         }
     }
 };
